d743bec77e3ad9c493b40b3560512b5419a5ef74
yamashitive <mail@nmpd.jp>
2020-09-17 13:39:03 +0900

    ひとまず、Hello World

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,9 @@
 const functions = require('firebase-functions');
 const express = require('express');
-const url = require('url');
 const app = express();
 
-const routes = {
-  '/page1': {
-    title: 'ページ1',
-    content: 'これはページ1です。',
-  },
-  '/page2': {
-    title: 'ページ2',
-    content: 'これはページ2です。',
-  },
-};
-
-app.set('view engine', 'ejs');
-app.engine('ejs', require('ejs').__express);
-
-app.get('*', function (req, res) {
-  const url_parts = url.parse(req.path);
-
-  if (routes[url_parts.pathname] == null) {
-    res.status(404).send('page not found');
-  } else {
-    res.status(200).render('template', {
-      title: routes[url_parts.pathname].title,
-      content: routes[url_parts.pathname].content,
-    });
-  }
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
 });
 
 exports.sample = functions.https.onRequest(app);
