7ccfbe17758246a7e7574922e60efb4a2dc5fb05
yamashitive <mail@nmpd.jp>
2020-09-17 16:09:20 +0900

    ルーティング設定

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,31 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
+const routes = {
+  '/page1': {
+    title: 'ページ1',
+    content: 'これはページ1です。',
+  },
+  '/page2': {
+    title: 'ページ2',
+    content: 'これはページ2です。',
+  },
+};
+
+app.set('view engine', 'ejs');
+
+app.get('*', function (req, res) {
+  const url_parts = req.path;
+  // res.send(url_parts);
+
+  if (routes[url_parts] == null) {
+    res.status(404).send('page not found');
+  } else {
+    res.status(200).render('./template.ejs', {
+      title: routes[url_parts].title,
+      content: routes[url_parts].content,
+    });
+  }
 });
 
 exports.sample = functions.https.onRequest(app);
